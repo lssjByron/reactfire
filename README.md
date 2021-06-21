0dcf7f404eae198600ef36523db5d613e48b46a6
byron <broby@IDM-PC-0066.IDM.Local>
2021-06-21 09:51:50 -0400

    fixed loading and navigations

---
+++ README.md
@@ -0,0 +1,22 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+Enter the project directory and run
+
+### `npm install`
+
+Will build all the dependencies for the project
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+### `npm start`
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The website is running at: https://anotherproj-d78c0.web.app/
---
+++ src/index.tsx
@@ -0,0 +1,42 @@
+import React, {Suspense} from "react";
+import { FirebaseAppProvider } from 'reactfire';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import theme from './theme';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_KrSJ7JtCI7BpHpCYSAczsYyGndD9Bgk",
+  authDomain: "anotherproj-d78c0.firebaseapp.com",
+  projectId: "anotherproj-d78c0",
+  storageBucket: "anotherproj-d78c0.appspot.com",
+  messagingSenderId: "61411529454",
+  appId: "1:61411529454:web:15d47c50b1388d192719ff",
+  measurementId: "G-Z9WSTX3N14"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+app.auth()
+app.firestore()
+ReactDOM.render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense>
+  <Suspense fallback={<div>Loading...</div>}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Suspense>
+  </FirebaseAppProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
